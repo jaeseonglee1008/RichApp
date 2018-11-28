1af59cf7c4beb69d813b8f0ebf348e791357961f
jason <jason@jasons-MacBook-Pro.local>
2018-11-28 00:01:38 -0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by jason on 11/28/18.
+//  Copyright © 2018 Jason Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
